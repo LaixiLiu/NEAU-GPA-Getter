d43f4290e31894827832579ed69c48f210437ac7
Laixi Liu <liulaixi.byte@foxmail.com>
2024-10-05 20:45:05 +0800

    chore: add sqlx, tokio, dotenv, and tauri-plugin-sql dependencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod csv_parser;
+use tauri::Manager;
 
-fn main() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
+mod api;
+
+#[tokio::main]
+async fn main() {
+    let app = tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::default().build())
+        .build(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    let db = api::setup_db(&app).await;
+    app.manage(api::AppState { db });
+    app.run(|_, _| {});
 }
