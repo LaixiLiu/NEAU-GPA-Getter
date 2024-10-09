489d8bf169e8825d723d9b41a8f4a3080a3a1d06
Laixi Liu <liulaixi.byte@foxmail.com>
2024-10-09 20:20:25 +0800

    feat: add support for setup database and initialize the searcher

--- src-tauri/src/api.rs
+++ src-tauri/src/api.rs
@@ -1,9 +1,13 @@
-use std::{error::Error, path};
+use std::{
+    path::{self, PathBuf},
+    sync::{Arc, Mutex},
+};
 
 use db::init_db;
 use sqlx::{Pool, Sqlite};
 use student::ParsedStudentData;
-use tauri::{App, Manager};
+use tauri::{AppHandle, Manager};
+use tauri_plugin_dialog::DialogExt;
 
 mod csv_processor;
 mod db;
@@ -13,28 +17,69 @@ pub struct AppState {
     pub db: Pool<Sqlite>,
 }
 
-pub async fn setup_db(app: &App) -> Pool<Sqlite> {
+pub async fn setup_db(app: &AppHandle) {
     // init db
     let mut path = app
         .path()
         .data_dir()
         .expect("Failed to get the data directory");
-    init_db(&mut path).await.unwrap()
+    let db = init_db(&mut path).await.expect("Failed to init db");
+    app.manage(AppState { db });
 }
 
-pub async fn init_searcher(
+async fn pick_folder_dialog(app: tauri::AppHandle) -> Option<path::PathBuf> {
+    let directory_path = Arc::new(Mutex::new(None));
+
+    // clone the directory_path which will be used in closure
+    let directory_path_clone = Arc::clone(&directory_path);
+
+    // spawn a new async thread
+    let handle = tokio::spawn(async move {
+        let selected_path = app.dialog().file().blocking_pick_folder();
+        let mut dir = directory_path_clone
+            .lock()
+            .expect("Failed to lock directory path when selecting directory");
+        *dir = selected_path;
+    });
+
+    // await the task
+    handle
+        .await
+        .expect("Failed to await the selecting directory task");
+
+    // get the value
+    let dir_path = directory_path
+        .lock()
+        .expect("Failed to lock directory path when getting the value");
+    match &*dir_path {
+        Some(path) => Some(PathBuf::from(path.to_string())),
+        None => None,
+    }
+}
+
+#[tauri::command]
+pub async fn initialize_searcher(
     state: tauri::State<'_, AppState>,
-    path: &str,
-) -> Result<(), Box<dyn Error>> {
+    app: tauri::AppHandle,
+) -> Result<(), String> {
     let db = &state.db;
+
+    let path: PathBuf = match pick_folder_dialog(app).await {
+        Some(val) => val,
+        None => return Ok(()),
+    };
     // parse csv
-    let data = csv_processor::extract_data_from_files(path)?;
+    let data = csv_processor::extract_data_from_files(&path).map_err(|err| err.to_string())?;
     // set db
     // TODO: use multiple threads to insert data
     for (sid, ParsedStudentData { student, records }) in data {
-        db::insert_or_update_student(db, &student).await?;
+        db::insert_or_update_student(db, &student)
+            .await
+            .map_err(|err| err.to_string())?;
         for record in records {
-            db::insert_academic_record(db, &sid, &record).await?;
+            db::insert_academic_record(db, &sid, &record)
+                .await
+                .map_err(|err| err.to_string())?;
         }
     }
 
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,22 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
-use tokio::runtime;
+use tauri::{AppHandle, Manager};
 
 mod api;
 
 fn main() {
     let app = tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![api::initialize_searcher])
         .plugin(tauri_plugin_sql::Builder::default().build())
+        .setup(|app| {
+            let handle = app.handle();
+            tauri::async_runtime::block_on(api::setup_db(handle));
+            Ok(())
+        })
         .build(tauri::generate_context!())
         .unwrap();
-
-    let db = runtime::Runtime::new()
-        .unwrap()
-        .block_on(api::setup_db(&app));
-
-    app.manage(api::AppState { db });
 
     app.run(|_, _| {});
 }
